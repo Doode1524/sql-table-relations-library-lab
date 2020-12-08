0dd77f3ae925539af742323a227026a67d0171c1
Doode1524 <Doode1524@gmail.com>
2020-12-07 19:23:59 -0500

    Done.

---
+++ lib/update.sql
@@ -0,0 +1,12 @@
+UPDATE characters
+SET species = "Martian" 
+WHERE id = 8
+
+-- SELECT
+-- SUBSTR(characters, 0, LENGTH(characters) - 1)
+-- FROM characters
+-- UPDATE characters
+-- SET species = "Martian" 
+-- WHERE SUBSTR(characters, 0, LENGTH(characters) - 1)
+
+
